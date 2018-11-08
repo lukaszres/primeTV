4f94d6810a6db18eff18877940a7268d40db0ced
lukasz.reszka <lukasz.reszka@finanteq.com>
2018-11-08 10:38:46 +0100

    Added genres selector.

--- src/main/java/com/lkre/web/index/IndexBacking.java
+++ src/main/java/com/lkre/web/index/IndexBacking.java
@@ -1,11 +1,5 @@
 package com.lkre.web.index;
 
-import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
 import com.lkre.dao.logger.Activity;
 import com.lkre.dao.logger.Logger;
 import com.lkre.dao.logger.Site;
@@ -15,14 +9,23 @@ import com.lkre.services.htmlService.Seance;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @ManagedBean
 @ViewScoped
 public class IndexBacking {
-
     private List<Seance> seances;
     private String seancesString;
+    private List<String> genres = new ArrayList<>();
+    private List<String> selectedGenres = new ArrayList<>();
     //    @ManagedProperty(value = "#{urlHtmlService}")
     private HtmlService htmlService = HtmlServiceFactory.createService();
     @ManagedProperty(value = "#{logger}")
@@ -36,15 +39,24 @@ public class IndexBacking {
     public void downloadSeances(ActionEvent e) {
 
         String details = e.getComponent()
-                          .getId();
+                .getId();
         logger.log(Site.INDEX, Activity.PUSH_BUTTON, details);
 
         seances = htmlService.main();
         StringBuilder stringBuilder = new StringBuilder();
         seances.forEach(seance -> stringBuilder.append(seance.toString())
-                                               .append("\n")
+                .append("\n")
         );
         seancesString = stringBuilder.toString();
+        createGenres();
+    }
 
+    private void createGenres() {
+        seances.forEach(seance -> {
+            String genre = seance.getGenre();
+            if (!genres.contains(genre)) {
+                genres.add(genre);
+            }
+        });
     }
 }
