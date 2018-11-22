8bf0cb13ae400040ba1a692906e8b1166680ef56
lkre <lukasz.res@gmail.com>
2018-11-22 16:10:16 +0100

    Revert "Merge branch 'develop'"
    This reverts commit c5316c36cab46734e4fc46cefac4f98b11bbbc3e, reversing
    changes made to cae0bc1b6225f737fb0941568a6f2f1562af0925.

--- src/main/java/com/lkre/web/common/InfoBean.java
+++ src/main/java/com/lkre/web/common/InfoBean.java
@@ -1,31 +1,23 @@
 package com.lkre.web.common;
 
-import com.lkre.dao.logger.Logger;
-import launch.Main;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
 import java.io.File;
 import java.io.IOException;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import com.lkre.dao.logger.Logger;
+import launch.Main;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
 @ManagedBean
 @ViewScoped
 public class InfoBean {
-    private final String COOKIE_MANIFEST = "We use cookies to personalise content and ads, " +
-            "to provide social media features and to analyse our traffic. We also share " +
-            "information about your use of our site with our social media, advertising and " +
-            "analytics partners who may combine it with other information that you’ve provided to" +
-            " them or that they’ve collected from your use of their services. You consent to our " +
-            "cookies if you continue to use our website.";
-
     @ManagedProperty(value = "#{logger}")
     private Logger logger;
     private String version;
@@ -41,7 +33,7 @@ public class InfoBean {
 
     private String loadManifest(String manifestEntry) {
         String absolutePath = Main.getRootFolder()
-                .getAbsolutePath();
+                                  .getAbsolutePath();
         File file = new File(absolutePath + "/target/embeddedTomcatSample.jar");
         try {
             JarFile jar = new java.util.jar.JarFile(file);
--- src/main/java/com/lkre/web/index/IndexBacking.java
+++ src/main/java/com/lkre/web/index/IndexBacking.java
@@ -3,7 +3,6 @@ package com.lkre.web.index;
 import com.lkre.dao.logger.Activity;
 import com.lkre.dao.logger.Logger;
 import com.lkre.dao.logger.Site;
-import com.lkre.services.cookieService.CookieHandler;
 import com.lkre.services.htmlService.HtmlService;
 import com.lkre.services.htmlService.HtmlServiceFactory;
 import com.lkre.services.htmlService.Seance;
@@ -16,15 +15,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
-import javax.servlet.http.Cookie;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static com.lkre.web.common.cookies.CookieExpiration.MONTH;
-import static com.lkre.web.common.cookies.CookieExpiration.WEEK;
-import static com.lkre.web.common.cookies.Cookies.ACCEPTED_COOKIES;
-import static com.lkre.web.common.cookies.Cookies.GENRES_COOKIES;
 
 @Getter
 @Setter
@@ -39,16 +32,10 @@ public class IndexBacking {
     private HtmlService htmlService = HtmlServiceFactory.createService();
     @ManagedProperty(value = "#{logger}")
     private Logger logger = new Logger();
-    private boolean isCookiesAccepted;
 
     @PostConstruct
     public void init() {
         logger.log(Site.INDEX, Activity.OPEN_SITE, null);
-        Cookie acceptedCookie = CookieHandler.getCookie(ACCEPTED_COOKIES);
-        isCookiesAccepted = false;
-        if (acceptedCookie != null) {
-            isCookiesAccepted = acceptedCookie.getValue().equalsIgnoreCase("true");
-        }
     }
 
     public void downloadSeances(ActionEvent e) {
@@ -58,8 +45,6 @@ public class IndexBacking {
         seances = htmlService.downloadSeances();
         createSeancesString();
         createGenres();
-        importSelectedGenres();
-        updateTextArea();
     }
 
     private void createSeancesString() {
@@ -90,56 +75,8 @@ public class IndexBacking {
     }
 
     public void onSelectGenresChanged() {
-        CookieHandler.setCookie(GENRES_COOKIES, selectedGenresString(), MONTH);
-        updateTextArea();
-    }
-
-    private void updateTextArea() {
         createSelectedSeances();
         createSeancesString();
         RequestContext.getCurrentInstance().update("form_textArea");
-    }
-
-    private String selectedGenresString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        selectedGenres.forEach(genre -> stringBuilder.append(prepareGenreString(genre)));
-        String s = stringBuilder.toString();
-        int length = s.length();
-        if (length > 0) {
-            s = s.substring(0, s.length() - 1);
-        }
-        return s;
-    }
-
-    private String prepareGenreString(String genre) {
-        if (genre != null && genre.length() > 0)
-            return genre.replace(" ", "_").replace(";", "_:_") + "|";
-        else
-            return "";
-    }
-
-    private void importSelectedGenres() {
-        Cookie cookie = CookieHandler.getCookie(GENRES_COOKIES);
-        selectedGenres = prepareGenreList(cookie);
-    }
-
-    private List<String> prepareGenreList(Cookie cookie) {
-        if (cookie != null) {
-            String selectedGenresString = cookie.getValue();
-            String[] split = selectedGenresString.split("\\|");
-            List<String> selectedGenresList = new ArrayList<>();
-            for (String genre : split) {
-                String replace = genre.replace("_:_", ";").replace("_", " ");
-                selectedGenresList.add(replace);
-            }
-            return selectedGenresList;
-        } else
-            return null;
-    }
-
-    public void onAcceptCookie(ActionEvent e) {
-
-        isCookiesAccepted = true;
-        CookieHandler.setCookie(ACCEPTED_COOKIES, "true", WEEK);
     }
 }
